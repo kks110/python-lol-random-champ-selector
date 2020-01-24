891e0746e3653573f9dcdfa2a356bb213f13c02a
Kelvin Samuel <kelvin.samuel@simplybusiness.co.uk>
2020-01-24 16:45:20 +0000

    now loops until there is a winner

--- bracket_creator.py
+++ bracket_creator.py
@@ -3,44 +3,50 @@ import random_champ_generator
 import file_io
 
 
-def get_players():
+def get_players(player_count, champ_count):
     player_list = []
-    while True:
-        player = input('Please enter the players name: ')
+    counter = 1
+    for x in range(int(player_count)):
+        player = input('Please enter player %s: ' % counter)
         player_list.append(player)
-        keep_going = input('Would you like to add another player? (Y/n): ')
-        if keep_going == 'n':
-            break
-    player_bracket = create_bracket(player_list)
+        counter += 1
+    player_bracket = create_bracket(player_list, int(champ_count))
     file_io.save_bracket(player_bracket)
 
 
-def create_bracket(player_list):
+def round_complete(bracket, champ_count):
+    player_list = []
+    counter = 1
+    if len(bracket) == 1:
+        player_bracket = {}
+        file_io.save_bracket(player_bracket)
+        return
+    for x in range(len(bracket)):
+        game_winner = input('Who won game %s: ' % counter)
+        player_list.append(game_winner)
+        counter += 1
+    player_bracket = create_bracket(player_list, int(champ_count))
+    file_io.save_bracket(player_bracket)
+
+
+def create_bracket(player_list, champ_count):
     if len(player_list) % 2 != 0:
         player_list.append('Bye')
     random.shuffle(player_list)
-    match_count = len(player_list)-1
     bracket = {}
-
-    for x in range(match_count):
-        match = {
-            'player_1': {
-                'name': '',
-                'champs': random_champ_generator.generate_champs(3)
-            },
-            'player_2': {
-                'name': '',
-                'champs': random_champ_generator.generate_champs(3)
-            },
-        }
-        bracket['match_%s' % str(int(x)+1)] = match
 
     counter = 1
     while player_list:
-        bracket['match_%s' % counter]['player_1']['name'] = player_list.pop(0)
-        bracket['match_%s' % counter]['player_2']['name'] = player_list.pop(0)
+        match = {
+            'player_1': {
+                'name': player_list.pop(0),
+                'champs': random_champ_generator.generate_champs(champ_count)
+            },
+            'player_2': {
+                'name': player_list.pop(0),
+                'champs': random_champ_generator.generate_champs(champ_count)
+            },
+        }
+        bracket['match_%s' % str(counter)] = match
         counter += 1
     return bracket
-
-
-get_players()
