74eaf34c02367cd3bdb099897720e58bbe23d28c
Kelvin Samuel <kelvin.samuel@simplybusiness.co.uk>
2020-01-24 16:46:44 +0000

    allowed gui builder to handle multiple amount of champs:

--- gui_builder.py
+++ gui_builder.py
@@ -1,13 +1,14 @@
 
 
-def match_builder(match_number, player_1_details, player_2_details):
+def match_builder(match_number, match_details):
     print('_________________________________________________')
     print('| Match %s:                                      |' % match_number)
     print('|-----------------------------------------------|')
-    print('| Player: %s| Player: %s|' % (space_calc(player_1_details['player_1']['name']), space_calc(player_2_details['player_2']['name'])))
-    print('| Champs: %s| Champs: %s|' % (space_calc(player_1_details['player_1']['champs'][0]), space_calc(player_2_details['player_2']['champs'][0])))
-    print('|         %s|         %s|' % (space_calc(player_1_details['player_1']['champs'][1]), space_calc(player_2_details['player_2']['champs'][1])))
-    print('|         %s|         %s|' % (space_calc(player_1_details['player_1']['champs'][2]), space_calc(player_2_details['player_2']['champs'][2])))
+    print('| Player: %s| Player: %s|' % (space_calc(match_details['player_1']['name']), space_calc(match_details['player_2']['name'])))
+    print('| Champs: %s| Champs: %s|' % (space_calc(match_details['player_1']['champs'][0]), space_calc(match_details['player_2']['champs'][0])))
+    if len(match_details['player_1']['champs']) > 1:
+        for x in range(len(match_details['player_1']['champs'])-1):
+            print('|         %s|         %s|' % (space_calc(match_details['player_1']['champs'][int(x)+1]), space_calc(match_details['player_2']['champs'][int(x)+1])))
     print('|_______________________|_______________________|')
     print()
 
