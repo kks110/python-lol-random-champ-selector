fd726422617358cc9644765f6d8daee8b43e4e68
Kelvin Samuel <kelvin@kks110.com>
2020-01-25 11:41:10 +0000

    align files names

--- lol_1v1_tournament_generator.py
+++ lol_1v1_tournament_generator.py
@@ -1,7 +1,7 @@
 import setup_helper
 import file_io
-import bracket_creator
-import gui_builder
+import bracket_generator
+import gui_generator
 
 
 def setup():
@@ -12,15 +12,15 @@ def setup():
 def app():
     player_count = input('How many players?: ')
     champ_count = input('How many champs per player?: ')
-    bracket_creator.get_players(player_count, champ_count)
+    bracket_generator.get_players(player_count, champ_count)
     bracket = file_io.load_bracket()
     while len(bracket) > 0:
         counter = 1
         for x in range(len(bracket)):
-            gui_builder.match_builder(int(x)+1, bracket['match_%s' % counter])
+            gui_generator.match_builder(int(x) + 1, bracket['match_%s' % counter])
             counter += 1
         print('Good Luck summoners\n\n')
-        bracket_creator.round_complete(bracket, champ_count)
+        bracket_generator.round_complete(bracket, champ_count)
         bracket = file_io.load_bracket()
 
 
